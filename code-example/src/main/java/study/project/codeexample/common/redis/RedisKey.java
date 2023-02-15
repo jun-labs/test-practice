ffcad1809e71c6d8ea695914b49f6739ad50291c
devjun10 <devjun1023@gmail.com>
2023-02-16 05:38:06 +0900

    레디스 설정 추가
    - 레디스 캐시 사용을 위한 설정 추가

---
+++ code-example/src/main/java/study/project/codeexample/common/redis/RedisKey.java
@@ -0,0 +1,8 @@
+package study.project.codeexample.common.redis;
+
+public interface RedisKey {
+
+    String userCacheValue = "userCache";
+    String userCacheKey = "#userId";
+    String userCacheKey2 = "#name";
+}
